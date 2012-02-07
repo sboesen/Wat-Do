c9af611899e2fa4f2c5fb2f17336cffd22b9add0
Stefan Boesen <stefan.boesen@gmail.com>
2012-02-06 22:40:02 -0800

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Watdo::Application.config.secret_token = '57fd56e0b48ebec247929f46187bce5772696503baa096df0d570061b21f4563ee4bc1885ad02a955543d01d94c82b3b9f61e71950693a4a3fe4ee8175b47061'
