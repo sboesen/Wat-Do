a1ebdb97586f4fcc23385268acd7eb4eaba38b43
Stefan Boesen <stefan.boesen@gmail.com>
2012-03-15 17:48:48 -0700

    Updated css and js. Moved bootstrap.

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -15,7 +15,56 @@ $(document).ready(function() {
       $('.popover-title').text(date.toString());
     })
   });
+  $('#task_description').keypress(eventHandler);
+  $('#task_place').keypress(eventHandler);
+  $('#task_date').keypress(eventHandler);
+  $('button').click(function(event) {
+    console.log("Got click")
+    var buttonClicked = $(this);
+    if (buttonClicked.hasClass('active')) return;
+    event.preventDefault();
+    $('button').removeClass('active');
+    buttonClicked.addClass('active');
+    $('.input-append > input:visible').toggle({duration: 300}); //hide
+    var derp = buttonClicked.next('input')
+    console.log(derp)
+    derp.toggle({duration: 300}).focus(); //show
+  });
 });
+
+
+
+
+
+
+function eventHandler(event) {
+  var char = String.fromCharCode(event.keyCode);
+  if (char == '!') {
+    //! sign. So... go to task_description
+    event.preventDefault();
+    $('button').removeClass('active');
+    $('#btn-description').addClass('active');
+    $(this).toggle({duration: 300});//hide
+    $('#task_description').toggle({duration: 300}).focus();;//show
+  }
+  else if (char == '@') {
+    //@ sign. So... go to task_date
+    event.preventDefault();
+    $('button').removeClass('active');
+    $('#btn-date').addClass('active');
+    $(this).toggle({duration: 300});//hide  
+    $('#task_date').toggle({duration: 300}).focus();//show
+
+  }
+  else if (char == '#') {
+    //# sign. So... go to task_place
+    event.preventDefault();
+    $('button').removeClass('active');
+    $('#btn-place').addClass('active');
+    $(this).toggle({duration: 300});//hide
+    $('#task_place').toggle({duration: 300}).focus();;//show
+  }
+}
 
 function renderContent() {
   output = '<input type="text" class="input-large popover-input" id="popover-input" placeholder="Enter date"> <br />'
