b7180e2efc0342b30ca30667782f3deadf198900
Stefan Boesen <stefan.boesen@gmail.com>
2012-06-01 01:28:28 -0700

    Removed dumb background settings, added ellipses if task description is too long to display.

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,12 @@ class Task < ActiveRecord::Base
 	belongs_to :user
 	validates :description, presence: true
 	validates :user_id, presence: true
+
+  def desc_display
+    if self.description.length >= 56
+      self.description[0..53] + '...'
+    else
+      self.description
+    end
+  end
 end
