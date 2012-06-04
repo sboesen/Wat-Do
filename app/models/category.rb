0bd9d046310ea926e5792c9f6378e5f849587454
Stefan Boesen <stefan.boesen@gmail.com>
2012-06-04 10:51:56 -0700

    Validating presence of columns for categories.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,8 @@
 class Category < ActiveRecord::Base
   belongs_to :color, class_name: "Color", foreign_key: :color_id
   belongs_to :user
+
+  validates :color_id, presence: true
+  validates :user_id, presence: true
+  validates :name, presence: true
 end
