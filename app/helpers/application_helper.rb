411bc29de91c61191eb5b246a7955c97d2913c8b
Stefan Boesen <stefan.boesen@gmail.com>
2012-06-05 14:24:49 -0700

    Remove obvious comment.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,5 @@
 module ApplicationHelper
   def active_menu_items
-    #get all the active menu items
     #params[:controller] == 'tasks'
     #params[:action] == 'index'
     case params[:controller]
